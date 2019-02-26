68bef890208132be167bc4bf3bd7b61265cb98cf
not ervin <notminin@pm.me>
2019-02-26 10:16:11 +0000

    add physics layers

--- src/ai.js
+++ src/ai.js
@@ -12,40 +12,49 @@ canvas.width = document.body.scrollWidth;
 canvas.height = document.body.scrollHeight;
 document.body.appendChild(canvas);
 
-// create engine
-let engine = Engine.create();
+// ai configuration
+let layers = 9;
+let iterations = 3;
 
-engine.world.gravity.scale = 0;
+// create engines
+let engines = [];
 
-let runner = Runner.create({
-	delta: 1000 / 30,
-	isFixed: false,
-	enabled: true,
-});
+for (let i = 0; i < layers; i++) {
+	let engine = Engine.create();
+	engines.push(engine);
+	
+	engine.world.gravity.scale = 0;
+	
+	let runner = Runner.create({
+		delta: 1000 / 30,
+		isFixed: false,
+		enabled: true,
+	});
 
-// create bodies
-let walls = [
-	Bodies.rectangle(250, 100, 300, 25, { isStatic: true }),
-	Bodies.rectangle(100, 350, 25, 500, { isStatic: true }),
-	Bodies.rectangle(250, 600, 300, 25, { isStatic: true }),
-	Bodies.rectangle(400, 350, 25, 500, { isStatic: true }),
-]
+	// create world
+	let walls = [
+		Bodies.rectangle(250, 100, 300, 25, { isStatic: true }),
+		Bodies.rectangle(100, 350, 25, 500, { isStatic: true }),
+		Bodies.rectangle(250, 600, 300, 25, { isStatic: true }),
+		Bodies.rectangle(400, 350, 25, 500, { isStatic: true }),
+	]
+	
+	World.add(engine.world, walls);
+	
+	let balls = Composites.pyramid(165, 275, 9, 8, 0, -40, (x, y) => {
+		return Bodies.circle(x, y, 10, { label: y == 235 ? "eight" : "ball", render: { fillStyle: y == 235 ? "#000": undefined } });
+	});
+	
+	World.add(engine.world, balls);
+	
+	let poolBall = Bodies.circle(250, 500, 10, { label: "ball", render: { fillStyle: "#fff" } });
+	World.add(engine.world, poolBall);
 
-World.add(engine.world, walls);
+	Body.applyForce(poolBall, poolBall.position, { x: -(Math.random() / 100), y: -(Math.random() / 100) });
 
-let balls = Composites.pyramid(165, 275, 9, 8, 0, -40, (x, y) => {
-	return Bodies.circle(x, y, 10, { label: y == 235 ? "eight" : "ball", render: { fillStyle: y == 235 ? "#000": undefined } });
-});
-
-World.add(engine.world, balls);
-
-let poolBall = Bodies.circle(250, 500, 10, { label: "ball", render: { fillStyle: "#fff" } });
-World.add(engine.world, poolBall);
-
-Body.applyForce(poolBall, poolBall.position, { x: 0, y: -0.015 })
-
-// start engine
-Runner.run(runner, engine);
+	// start
+	Runner.run(runner, engine);
+}
 
 // start rendering
 let ctx = canvas.getContext("2d");
@@ -53,37 +62,41 @@ ctx.lineWidth = 2;
 ctx.font = "1em Arial";
 
 (function render() {
-	let bodies = Composite.allBodies(engine.world);
-
 	window.requestAnimationFrame(render);
 
 	ctx.fillStyle = "#161621";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	for (let i = 0; i < bodies.length; i += 1) {
-		let body = bodies[i];
-		let vertices = body.vertices;
+	for (let i = 0; i < engines.length; i += 1) {
+		let engine = engines[i];
 
-		if (!body.render.visible) continue;
+		let bodies = Composite.allBodies(engine.world);
 
-		if (body.label == "eight" || body.label == "ball") {
-			ctx.beginPath();
+		for (let i = 0; i < bodies.length; i += 1) {
+			let body = bodies[i];
+			let vertices = body.vertices;
 
-			ctx.ellipse(body.position.x, body.position.y, body.circleRadius, body.circleRadius, 0, 0, Math.PI * 2);
-		} else {
-			ctx.beginPath();
-	
-			ctx.moveTo(vertices[0].x, vertices[0].y);
-	
-			for (let j = 1; j < vertices.length; j += 1) {
-				ctx.lineTo(vertices[j].x, vertices[j].y);
+			if (!body.render.visible) continue;
+
+			if (body.label == "eight" || body.label == "ball") {
+				ctx.beginPath();
+
+				ctx.ellipse(body.position.x, body.position.y, body.circleRadius, body.circleRadius, 0, 0, Math.PI * 2);
+			} else {
+				ctx.beginPath();
+		
+				ctx.moveTo(vertices[0].x, vertices[0].y);
+		
+				for (let j = 1; j < vertices.length; j += 1) {
+					ctx.lineTo(vertices[j].x, vertices[j].y);
+				}
+		
+				ctx.lineTo(vertices[0].x, vertices[0].y);
 			}
-	
-			ctx.lineTo(vertices[0].x, vertices[0].y);
-		}
-	
-		ctx.fillStyle = body.render.fillStyle;
+		
+			ctx.fillStyle = body.render.fillStyle;
 
-		ctx.fill();
+			ctx.fill();
+		}
 	}
 })();
